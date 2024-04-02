e9a48a16c9fa38fd2eac8212a677cb507ab83f02
Elchin <elcinceferli@gmail.com>
2024-04-03 01:44:40 +0400

    tables with sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+from models.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable = False)
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            city_instances = []
+            cities = storage.all(City)
+            for obj in cities.values():
+                if obj.state_id == self.id:
+                    city_instances.append(obj)
+            return city_instances
