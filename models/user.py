6fa8ecb94aff7ba771380f7c0cfb50b89c7c5d53
Elchin <elcinceferli@gmail.com>
2024-04-03 16:13:17 +0400

    hot fix

--- models/user.py
+++ models/user.py
@@ -12,4 +12,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship('Place', cascade="add, delete", backref='user')
+    places = relationship('Place', cascade="all, delete", backref='user')
