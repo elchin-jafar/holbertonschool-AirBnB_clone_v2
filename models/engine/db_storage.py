c9e4fea36fb08b57a46d4f2593d0b867bf543357
Elchin <elcinceferli@gmail.com>
2024-04-03 04:06:07 +0400

    plz

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from models.base_model import Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage:
     __engine = None
     __session = None
+    classes = {'User': User,
+               'Place': Place,
+               'State': State,
+               'City': City,
+               'Amenity': Amenity,
+               'Review': Review
+               }
 
     def __init__(self):
         self.user = os.getenv('HBNB_MYSQL_USER')
@@ -19,3 +33,34 @@ class DBStorage:
                                       pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        result = {}
+
+        if cls:
+            class_obj = self.classes.get(cls)
+            if class_obj:
+                for instance in self.__session.query(class_obj).all():
+                    key = f"{cls}.{instance.id}"
+                    result[key] = instance
+        else:
+            for class_name, class_obj in self.classes.items():
+                for instance in self.__session.query(class_obj).all():
+                    key = f"{class_name}.{instance.id}"
+                    result[key] = instance
+        return result
+    
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)
--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City")
+        cities = relationship("City", cascade="all", backref="state")
     else:
         @property
         def cities(self):
