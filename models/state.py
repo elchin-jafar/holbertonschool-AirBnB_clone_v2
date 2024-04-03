987ed8a968a9f02ee35651c11153ccbb390c551a
Elchin <elcinceferli@gmail.com>
2024-04-03 14:35:12 +0400

    cascade delete

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all", backref="state")
+        cities = relationship("City", cascade="all, delete", backref="state")
     else:
         @property
         def cities(self):
