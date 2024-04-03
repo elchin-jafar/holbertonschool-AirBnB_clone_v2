2d8af93942424cf3d34f2e03d868253a08944a7a
Elchin <elcinceferli@gmail.com>
2024-04-03 20:00:20 +0400

    add tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -15,5 +15,4 @@ class test_Amenity(test_basemodel):
 
     def test_name2(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(self.amenity1.name), str)
