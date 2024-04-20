24b434713307e45da30d08271780ba14c83cd382
elchin-jafar <elcinceferli@gmail.com>
2024-04-20 15:35:04 +0000

    fix err

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     states = list(storage.all(State).values())
-    result = sorted(obj, key=lambda x: x.name)
+    result = sorted(states, key=lambda x: x.name)
     return render_template("7-states_list.html", states=result)
 
 
@@ -21,4 +21,5 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="0.0.0.0", port=5000)
